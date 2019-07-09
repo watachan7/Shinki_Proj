4a72f6785bad9930f39a320117448f4307268dad
develop47 <develop47@develop47noiMac.local>
2019-07-09 17:09:25 +0900

    source fix

--- myword.py
+++ myword.py
@@ -1,6 +1,7 @@
+#coding: utf-8
 import speech_recognition
 
-print('Please input your message.')
+print('Please input your message.\nIf you want to exit, please enter "exit".')
 
 while True:
 	user_message = input('>> ')
@@ -8,7 +9,6 @@ while True:
 	# exitが入力された時、プログラム終了
 	if ( user_message == "exit" ):
 		break
-
 
 	result_message = user_message
 	print(result_message)
