5b89c2e38ab5b98713fa9a94ad577f77ed44e45b
develop47 <develop47@develop47noiMac.local>
2019-07-11 18:14:28 +0900

    source init

---
+++ audiotest.py
@@ -0,0 +1,31 @@
+import numpy as np
+import wave 
+import pyaudio
+import matplotlib.pyplot as plt
+
+def PlayWavFie(Filename = "sample.wav"):
+    try:
+        wf = wave.open(Filename, "r")
+    except FileNotFoundError: #ファイルが存在しなかった場合
+        print("[Error 404] No such file or directory: " + Filename)
+        return 0
+        
+    # ストリームを開く
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    # 音声を再生
+    chunk = 1024
+    data = wf.readframes(chunk)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+    stream.close()
+    p.terminate()
+
+
+if __name__ is "__main__":
+    PlayWavFie("sample.wav") 
