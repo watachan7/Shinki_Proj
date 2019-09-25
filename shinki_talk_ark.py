2c15edd808452f4cc74021926c95d3072d7c1d06
develop47 <develop47@develop47noiMac.local>
2019-09-25 18:01:45 +0900

    shinki talk new make

--- shinki_talk_ark.py
+++ shinki_talk_ark.py
@@ -1,24 +1,15 @@
+# coding: utf-8
+import ark_brain
 
-shinki_name_data = open('Shinki/shinki_list.txt', 'r')
-
-print('Please choose youre shinki.')
+print('Talk Start.')
 
 while True:
-	request_shinki_name = input('-> ')
+	user_message = input('-> ')
 
-	# exitが入力された時、プログラム終了
-	if ( request_shinki_name == 'exit' ):
+	if ( user_message == 'exit' ):
 		break
 
-	lines = shinki_name_data.readlines()
-
-	for line in lines:
-		print(line)
-		if line != request_shinki_name:
-			print('There is no such shinki name.')
-
-	result_message = request_shinki_name
+	result_message = ark_brain.main(user_message)
 	print(result_message)
 
 print('program ends.')
-shinki_name_data.close()
